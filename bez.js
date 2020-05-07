cc3650e3bdf8be54ae02d6a313815be5e5cf331d
Philip Meyer <PhilipMeyer@Philips-MacBook-Pro-2.local>
2020-05-06 19:23:07 -0700

    shadowform object

--- bez.js
+++ bez.js
@@ -49,36 +49,115 @@ function writeLines (framecount, maxbezzes,strkOr=0,fillOr=0) {
 
 
 class ShadowForm {
-  constructor(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6) {
-    this.x1= x1;
-    this.y1= y1;
-    this.x2= x2;
-    this.y2= y2;
-    this.x3= x3;
-    this.y3= y3;
-    this.x4= x4;
-    this.y4= y4;
-    this.x5= x5;
-    this.y5= y5;
-    this.x6= x6;
-    this.y6= y6;
+  constructor(vertices, magbase, magrange, perlinc, ctrlscl) { //a1,a2,a3,a4,a5,m1,m2,m3,m4,m5
+    this.spacing = TWO_PI / vertices;
+    this.vertices = [];
+    this.controls = []
+    this.magrange = magrange;
+    this.magbase = magbase;
+    this.perlinc = perlinc;
+    this.ctrlscl = ctrlscl;
   }
 
-  show (number) {
-    for (let i=0; i < number; i++) {
-      // ellipse(width/2, height/2, 500-(5*i), 500-(5*i));
-      scale(1-i/number,1-i/number);
-      beginShape();
-      curveVertex(this.x1,this.y1);
-      curveVertex(this.x2,this.y2);
-      curveVertex(this.x3,this.y3);
-      curveVertex(this.x4,this.y4);
-      curveVertex(this.x1,this.y1);
-      curveVertex(this.x5,this.y5);
-      curveVertex(this.x6,this.y6);
-      curveVertex(this.x2,this.y2);
-      curveVertex(this.x2,this.y2);
-      endShape();
+  create () {
+    let xoff = 0;
+
+    //vertices
+    for(let a=0; a < TWO_PI; a += this.spacing) {
+      let perl = random();//noise(xoff);
+      let mag = this.magbase + perl * this.magrange;
+      console.log(xoff+" "+noise(xoff))
+      let vertex = p5.Vector.fromAngle(a,mag)
+      this.vertices.push(vertex);
+      xoff += this.perlinc
     }
+    console.log('vertices: '+this.vertices);
+
+    //control points
+    xoff = 50;
+    for(let a = 0; a < this.vertices.length; a++) {
+      let angle = this.vertices[a].heading() - (2 * noise(xoff)) * this.spacing/2;
+      let mag = this.vertices[a].mag() * this.ctrlscl * (noise(xoff) + 1);
+      let control = p5.Vector.fromAngle(angle,mag)
+      this.controls.push(control);
+      xoff += this.perlinc
+    }
+  }
+
+  get angles () {
+    let angles = {
+      vertices: [],
+      controls: []
+    }
+    for (let i = 0; i < this.vertices.length; i++) {
+      angles.vertices.push(this.vertices[i].heading());
+      angles.controls.push(this.controls[i].heading());
+    }
+    return angles;
+  }
+
+  updateVPoints () {
+    for (let i = 0; i < this.vertices.length; i++) {
+      point(this.vertices[i].x, this.vertices[i].y);
+    }
+  }
+
+  updateCPoints () {
+    for (let i = 0; i < this.controls.length; i++) {
+      point(this.controls[i].x, this.controls[i].y);
+    }
+  }
+
+  update (number) {
+    let xoff = 456;
+    let yoff = 7893;
+    beginShape();
+    for (let j = 0; j < number; j++) {
+      let perlx = noise(xoff);
+      let perly = noise(yoff);
+      scale(1 - perlx / number, 1 - perly / number);
+      //scale(1 - j/number, 1 - j/number)
+      beginShape();
+      for (let i = 0; i < this.vertices.length + 1; i++) {
+        if(i == 0) {
+          vertex(this.vertices[0].x, this.vertices[0].y)
+        } else if (i == this.vertices.length) {
+          quadraticVertex(this.controls[0].x, this.controls[0].y, this.vertices[0].x, this.vertices[0].y);
+        } else {
+          quadraticVertex(this.controls[i].x, this.controls[i].y, this.vertices[i].x, this.vertices[i].y);
+        }
+      }
+      endShape();
+      xoff += this.perlinc;
+      yoff += this.perlinc;
+    }
+
+    
+    // for (let i=0; i < number; i++) {
+    //   // ellipse(width/2, height/2, 500-(5*i), 500-(5*i));
+
+    //   beginShape();
+    //   vertex(this.pos.v1.x,this.pos.v1.y);
+    //   quadraticVertex(this.pos.v2c.x,this.pos.v2c.y,this.pos.v2.x,this.pos.v2.y);
+    //   quadraticVertex(this.pos.v3c.x,this.pos.v3c.y,this.pos.v3.x,this.pos.v3.y);
+    //   quadraticVertex(this.pos.v4c.x,this.pos.v4c.y,this.pos.v4.x,this.pos.v4.y);
+    //   quadraticVertex(this.pos.v1c.x,this.pos.v1c.y,this.pos.v1.x,this.pos.v1.y);
+
+    // }
+    // push();
+    // noStroke();
+    // fill(255,0,0);
+    // // text("1",this.pos.v1.x,this.pos.v1.y);
+    // // text("1c",this.pos.v1c.x,this.pos.v1c.y);
+    // // text("2",this.pos.v2.x,this.pos.v2.y);
+    // // text("2c",this.pos.v2c.x,this.pos.v2c.y);
+    // // text("3",this.pos.v3.x,this.pos.v3.y);
+    // // text("3c",this.pos.v3c.x,this.pos.v3c.y);
+    // // text("4",this.pos.v4.x,this.pos.v4.y);
+    // // text("4c",this.pos.v4c.x,this.pos.v4c.y);
+    // //text("5",this.pos.v5.x,this.pos.v5.y);
+    // // text("6",this.pos.v6.x,this.pos.v6.y);
+    // // text("7",this.pos.v7.x,this.pos.v7.y);
+    // pop();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,134 +1,65 @@
 //SETUP
 function preload() {
-	songData = loadJSON('data/covid_means.json');
-	
-	soundFormats('mp3');
-	song = loadSound('audio/5 covid MIX 2.0');
+	// songData = loadJSON('data/covid_means.json');
+	// soundFormats('mp3');
+	// song = loadSound('audio/5 covid MIX 2.0');
 }
 
 function setup() {
 	c = createCanvas(windowWidth, windowHeight);	
+	background(150);
 	
 	//config
 	writeSpeed = 24; //framerate
 
-	otTone = new DataStream(songData.otTone.EnergyMean,1);
-	outAC1 = new DataStream(songData.outside.AC1Mean,1);
-	outEner = new DataStream(songData.outside.EnergyMean,1);
-	radiodrone = new DataStream(songData.radiodrone.FrequencyMean,1);
-	phonem = new DataStream(songData.phonem.EnergyMean,1);
-
-	rotateAmt = 0; 
-	linesPerWrite = 1;
-	growShrinkAmt = 1;
-	
-	gXPos = width * 0.25; 
-	gYPos = height * 0.5;
-
-	growShrinkOn = true; //use growing and shrinking form?
-	initBezzes = 1; //how many lines to start with
-	initMaxBezzes = 200; // how many lines to grow to
-
-	//background initial
-	bgColor = {
-		red: 100,
-		green: 105,
-		blue: 112,
-		alpha: 123,
-	}
-
-	//bezier initial values
-		scaleX = 1.2;
-		scaleY = 1.3;
-		bez1 = {
-			vert: {
-				x1: width * 0.25,
-				y1: height * 0.5, 
-				cpx1: width*0.05,
-				cpy1: -height*0.1,
-				cpx2: width*0.45,
-				cpy2: -height*0.1,
-				x2: width * 0.75,
-				y2: height * 0.5,
-			},
-			stroke: {
-				red: 150,
-				green: 150,
-				blue: 150,
-				alpha: 127
-			},
-			fill: {
-				red: 150,
-				green: 150,
-				blue: 150,
-				alpha: 100,
-			}
-		}
+	// otTone = new DataStream(songData.otTone.EnergyMean,1);
+	// outAC1 = new DataStream(songData.outside.AC1Mean,1);
+	// outEner = new DataStream(songData.outside.EnergyMean,1);
+	// radiodrone = new DataStream(songData.radiodrone.FrequencyMean,1);
+	// phonem = new DataStream(songData.phonem.EnergyMean,1);
 
 	//initializations
-	pixelDensity(1);
 	loopState = false;
-	initState = true;
-	maxBezzes = initMaxBezzes;
+	initState = false;
 	frameRate(writeSpeed);
 	angleMode(DEGREES);
 	mouseX = width / 2;
 	mouseY = height / 2;	
-	ellipseX = mouseX;
-	ellipseY = mouseY;
 	ii = millis() * writeSpeed/60; //
+	let w = 100 * width;
 
-	// noLoop();
+	// ShadowForm(vertices, magbase, magrange, perlinc, ctrlscl)
+	//shadow1 = new ShadowForm(3,50,250,1,1.5);
+	shadow1 = new ShadowForm(3,250,50,1,1.7);
+	shadow1.create();
 
-	shadow1 = new ShadowForm(0,0,400,198,165,314,-114,269,194,-15,376,114);
-	//c.background(bgColor.red, bgColor.green, bgColor.blue, bgColor.alpha);
-
-
+	translate(width/2,height/2);
+	noiseDetail(25,0.9);
+	
+	strokeWeight(1);
+	stroke(255,255,255);
+	fill(255,255,255,0);
+	
+	shadow1.update(200);
 }
-
 
 //DRAW
 function draw() {
-		background(0);
-// 	loadPixels();
-//   for(x = 0; x < width; x++) {
-//     for(y = 0; y < height; y++) {
-//     let pR = (x + y * width) * 4;
-//     let pG = pR + 1;
-//     let pB = pR + 2
-// 		let pA = pR + 3;
-    
-//     pixels[pR] = bgColor.red;
-//     pixels[pG] = bgColor.green;
-// 		pixels[pB] = bgColor.blue+randomGaussian(-40,200);
-// 		pixels[pA] = randomGaussian(-50,50)+sin_(x/10,sin_(x+frameCount,1,0.0001,0.01),56,200);
-// 		//pixels[pA] = outEner.modulator(y/outEner.stream.length,0,0,0,255);
-// 		}
-// 	}
-//   updatePixels();	
-
+	// background(150);
 	if(!initState) {
-		mills = performance.now() - startTime;
-		timecode = Math.ceil(mills/16); //sync song to visual
+		//mills = performance.now() - startTime;
+		//timecode = Math.ceil(mills/16); //sync song to visual
 	
 		//drawing
-		if(frameCount % linesPerWrite == 0) {
+		strokeWeight(4);
+		stroke(255,0,0);
+		shadow1.updateVPoints();
+		
 			
-			noStroke();
-			translate(width*0.5,height*0.5);
-			//
-			// rotate(frameCount % 360);
-			shearX(sin_(ii,10,-50,50));
-			fill(150,150,outAC1.modulator(timecode,0,0,140,160),outEner.modulator(timecode,0,0,1,10));
-			
-			shadow1.show(250);
-				
-			if (timecode >= 	otTone.stream.length - 7*(1000/writeSpeed)) {
-				console.log(timecode); 
-				startPlay(song);
-			};
-	
+		// if (timecode >= 	otTone.stream.length - 7*(1000/writeSpeed)) {
+		// 	console.log(timecode); 
+		// 	startPlay(song);
+		// };
 		//if(showTimeCode) console.log(millisToTime(mills)+' | '+timecode+' | '+Math.floor(100 * timecode / otTone.stream.length)+'%' );
-		}
 	}	
 }
