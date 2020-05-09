26b5b83765d350f2b65c7d559981d7eea6befa2b
Philip Meyer <philipmeyer@Philips-Mac-mini.local>
2020-05-09 12:44:04 -0700

    shifting form

--- shadow.js
+++ shadow.js
@@ -1,5 +1,5 @@
 class ShadowForm {
-  constructor(vertices, magbase, magrange, octaves, falloff, perlinc, ctrlscl) {
+  constructor(centerx, centery, vertices, magbase, magrange, octaves, falloff, perlinc, ctrlscl) {
     this.spacing = TWO_PI / vertices;
     this.vertices = [];
     this.controls = [];
@@ -10,39 +10,38 @@ class ShadowForm {
     this.ctrlscl = ctrlscl;
     this.octaves = octaves;
     this.falloff = falloff;
+    this.center = createVector(centerx,centery);
   }
 
-  create () {
-    let xoff = 0;
+  create (xoff) {
 
     //vertices
     for(let a=0; a < TWO_PI; a += this.spacing) {
-      let perl = random();//noise(xoff);
+      let perl = noise(xoff);
       let mag = this.magbase + perl * this.magrange;
-      console.log(xoff+" "+noise(xoff))
-      let vertex = p5.Vector.fromAngle(a,mag)
+      let vertex = p5.Vector.fromAngle(a,mag);
       this.vertices.push(vertex);
       xoff += this.perlinc
     }
-    console.log('vertices: '+this.vertices);
+    
 
     //control points 1
-    xoff = 50;
+    let cp1off = xoff + 50;
     for(let a = 0; a < this.vertices.length; a++) {
-      let angle = this.vertices[a].heading() - (2 * noise(xoff)) * this.spacing/2;
-      let mag = this.vertices[a].mag() * this.ctrlscl * (noise(xoff) + 1);
+      let angle = this.vertices[a].heading() - (2 * noise(cp1off)) * this.spacing/2;
+      let mag = this.vertices[a].mag() * this.ctrlscl * (noise(cp1off) + 1);
       let control = p5.Vector.fromAngle(angle,mag)
       this.controls.push(control);
-      xoff += this.perlinc
+      cp1off += this.perlinc
     }
     //control points 2
-    xoff = 900;
+    let cp2off = xoff + 900;
     for(let a = 0; a < this.vertices.length; a++) {
-      let angle = this.vertices[a].heading() - (2 * noise(xoff)) * this.spacing/2;
-      let mag = this.vertices[a].mag() * this.ctrlscl * (noise(xoff) + 1);
+      let angle = this.vertices[a].heading() - (2 * noise(cp2off)) * this.spacing/2;
+      let mag = this.vertices[a].mag() * this.ctrlscl * (noise(cp2off) + 1);
       let control = p5.Vector.fromAngle(angle,mag)
       this.controls2.push(control);
-      xoff += this.perlinc
+      cp2off += this.perlinc
     }
   }
 
@@ -58,15 +57,15 @@ class ShadowForm {
     }
   }
 
+
   update (number) {
-    let xoff = 456;
-    let yoff = 7893;
-    beginShape();
-    for (let j = 0; j < number; j++) {
-      let perlx = noise(xoff);
-      let perly = noise(yoff);
-      scale(1 - perlx / number, 1 - perly / number);
-      //scale(1 - j/number, 1 - j/number)
+    // let xoff = 456;
+    // let yoff = 7893;
+    // for (let j = 0; j < number; j++) {
+    //   let perlx = noise(xoff);
+    //   let perly = noise(yoff);
+    //   scale(1 - perlx / number, 1 - perly / number);
+    //   //scale(1 - j/number, 1 - j/number)
       beginShape();
       for (let i = 0; i < this.vertices.length + 1; i++) {
         if(i == 0) {
@@ -78,9 +77,9 @@ class ShadowForm {
         }
       }
       endShape();
-      xoff += this.perlinc;
-      yoff += this.perlinc;
-    }
+      // xoff += 1;
+      // yoff += 1;
+    // }
   }
 
   get angles () {
@@ -109,5 +108,26 @@ class ShadowForm {
       angles.controls2.push(this.controls[i].mag());
     }
     return mags;
+  }
+}
+
+function writeLines (arr, framecount, maxlines) {
+  let startLine;
+  let numLines;
+  let len = arr.length;
+  
+  if(framecount < maxlines) {
+    numLines = framecount;
+    startLine = len - framecount;
+  } else if (len < maxlines) {
+    numLines = len;
+    startLine = 0;
+  } else {maxlines
+    numLines = maxlines;
+    startLine = len - maxlines;
+  }
+
+  for (let i = startLine; i < startLine + numLines; i++) {
+    arr[i].update(1);
   }
 }
--- sketch.js
+++ sketch.js
@@ -5,6 +5,16 @@ function preload() {
 	// song = loadSound('audio/5 covid MIX 2.0');
 }
 
+let shadow1;
+let shadows = [];
+let maxlines = 25; 
+let xoff = 0;
+let inc = 0.05;
+let xpos;
+let ypos;
+let easing = 0.1;
+
+
 function setup() {
 	c = createCanvas(windowWidth, windowHeight);	
 	background(150);
@@ -25,38 +35,61 @@ function setup() {
 	angleMode(DEGREES);
 	mouseX = width / 2;
 	mouseY = height / 2;	
+	xpos = width / 2;
+	ypos = height / 2;
+
 	ii = millis() * writeSpeed/60;
-	
-	// ShadowForm(vertices, magbase, magrange, octaves, falloff, perlinc, ctrlscl) {
-	shadow1 = new ShadowForm(3,200,50,4,0.5,0.01,1.2);
-	shadow1.create();
-
-	
-
 }
+
+
+
 
 //DRAW
 function draw() {
-	
 	background(150);
+
+
 	if(!initState) {
 		//mills = performance.now() - startTime;
 		//timecode = Math.ceil(mills/16); //sync song to visual
-	
+
+		// ShadowForm(centerx, centery, vertices, magbase, magrange, octaves, falloff, perlinc, ctrlscl)
+		// center point in x and y
+		//number of vertices, base vertex vector length, range of vertex vector modulation, 
+		//perlin noise octaves, perlin fallof, perlin increment, scaling coefficient for control point magnitude
+		translate(width/2, height/2); //followPointer('x',xpos,easing),followPointer('y',ypos,easing)
+		shad = new ShadowForm(0,0,3,100,100,2,7,0.2,0.7);
+		shad.create(xoff);
+		shadows.push(shad);
+		if(shadows.length > 1001) { 
+			let d = shadows.length - 1001;
+			for(i=0; i<d; i++) {
+				shadows.shift();
+			} 
+		};
+		xoff += inc;
+
+
 		//drawing
-		strokeWeight(4);
-		stroke(255,0,0);
+		//noiseDetail(10,0.6);  //THIS IS HAVING SOME SORT OF INFLUENCE I DON'T UNDERSTAND
+		
 
 
-		noiseDetail(10,0.6);  //THIS IS HAVING SOME SORT OF INFLUENCE I DON'T UNDERSTAND
-		translate(width/2,height/2);
-		
-		
+		//translate(followPointer('x',xpos,easing),followPointer('y',ypos,easing));
 		strokeWeight(1);
-		stroke(255,255,255);
+		stroke(255,255,255,50);
 		fill(255,255,255,0);
 		
-		shadow1.update(20);
+
+			background(150);
+
+			writeLines(shadows,frameCount-2, maxlines);
+
+	
+			// grow/shrink
+			// if(growShrinkOn) {
+			// 	growShrink(initMaxBezzes);
+			// };
 		
 			
 		// if (timecode >= 	otTone.stream.length - 7*(1000/writeSpeed)) {
