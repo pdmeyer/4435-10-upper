4f34975b85a2406351b95a66935956526d7f4a2f
pdmeyer <philip.d.meyer@gmail.com>
2021-01-01 16:38:22 -0800

    prepped for cargo

--- configs.js
+++ configs.js
@@ -45,9 +45,9 @@ const shadscl = 0.8;
 const shadinc = 1.9;
 
 //background
-const imgfile = 'test-452_res.jpg'; 
-const audiofile = '05_covid MIX 2.0_M3_256.mp3';
-const datafile = 'covid_trimmed.json';
+const imgfile = 'https://files.cargocollective.com/c700175/test-452_res.jpg'; 
+const audiofile = 'https://files.cargocollective.com/c700175/05_covid-MIX-2.0_M3_256.mp3';
+const datafile = 'https://files.cargocollective.com/c700175/covid_trimmed.json';
 const submatx = 0.5;
 const matxeasing = 0.1;
 
